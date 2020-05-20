d5baab36a1f1701a372c79f256def96ca398647a
SergioV645 <sergio.valderrama645@gmail.com>
2020-05-20 15:43:05 -0500

    .

--- LBD-HIDE&SEEK/src/co/edu/unbosque/model/Usuario.java
+++ LBD-HIDE&SEEK/src/co/edu/unbosque/model/Usuario.java
@@ -1,12 +1,12 @@
 package co.edu.unbosque.model;
 
 import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+//
+//import javax.mail.Message;
+//import javax.mail.Session;
+//import javax.mail.Transport;
+//import javax.mail.internet.InternetAddress;
+//import javax.mail.internet.MimeMessage;
 
 import co.edu.unbosque.model.persistence.UsuarioDAO;
 
@@ -19,33 +19,33 @@ public class Usuario {
 
 	}
 
-	public void enviarEmail(String email){
-		Properties props= new Properties();		
-		props.setProperty("mail.smtp.host", "smtp.gmail.com");
-		props.setProperty("mail.smtp.starttls.enable", "true");
-		props.setProperty("mail.smtp.port","587");
-		props.setProperty("mail.smtp.user", "LoveByDelivery@gmail.com");
-		props.setProperty("mail.smtp.auth", "true");
-		
-		Session session= Session.getDefaultInstance(props, null);
-		session.getProperties().put("mail.smtp.ssl.trust", "smtp.gmail.com");
-
-		
-		 try {
-			 MimeMessage mensaje = new MimeMessage(session);
-	         mensaje.setFrom(new InternetAddress("LoveByDelivery@gmail.com"));
-	         mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
-	         mensaje.setSubject("Ingreso HIDE&SEEK");
-	         mensaje.setText("Se registró como cliente exitosamente");
-	         
-	         Transport t= session.getTransport("smtp");
-	         t.connect("LoveByDelivery@gmail.com", "123asdf123");
-	         t.sendMessage(mensaje, mensaje.getAllRecipients());
-	         
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	public void enviarEmail(String email){
+//		Properties props= new Properties();		
+//		props.setProperty("mail.smtp.host", "smtp.gmail.com");
+//		props.setProperty("mail.smtp.starttls.enable", "true");
+//		props.setProperty("mail.smtp.port","587");
+//		props.setProperty("mail.smtp.user", "LoveByDelivery@gmail.com");
+//		props.setProperty("mail.smtp.auth", "true");
+//		
+//		Session session= Session.getDefaultInstance(props, null);
+//		session.getProperties().put("mail.smtp.ssl.trust", "smtp.gmail.com");
+//
+//		
+//		 try {
+//			 MimeMessage mensaje = new MimeMessage(session);
+//	         mensaje.setFrom(new InternetAddress("LoveByDelivery@gmail.com"));
+//	         mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+//	         mensaje.setSubject("Ingreso HIDE&SEEK");
+//	         mensaje.setText("Se registró como cliente exitosamente");
+//	         
+//	         Transport t= session.getTransport("smtp");
+//	         t.connect("LoveByDelivery@gmail.com", "123asdf123");
+//	         t.sendMessage(mensaje, mensaje.getAllRecipients());
+//	         
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 	public UsuarioDAO getU() {
 		return u;

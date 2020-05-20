1afdb3c2f2a3d98421963659bb99490d847c5afb
SergioV645 <sergio.valderrama645@gmail.com>
2020-05-20 15:39:18 -0500

    .

--- LBD-HIDE&SEEK/src/co/edu/unbosque/model/CreditoExc.java
+++ LBD-HIDE&SEEK/src/co/edu/unbosque/model/CreditoExc.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 public class CreditoExc extends Exception{
 	
 	public CreditoExc() {
-		super("El credito no puede ser menor a 0");
+		super("El credito no puede ser menor o igual a 0");
 	}
 
 }
